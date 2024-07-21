e3a1da205d85824b8664a2ae6fd47c32163dd891
Shubham Ghanghor <128879210+Ghanghor@users.noreply.github.com>
2024-07-21 10:03:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.style.backgroundColor = '#555';
+    } else {
+      navbar.style.backgroundColor = '#eaa0a2';
+    }
+  });
+  
